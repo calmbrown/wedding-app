cc6f36510134eac7ab44813009c8dede885aca41
taemin <taemin@taemins-MacBook-Air.local>
2026-04-20 16:08:59 +0900

    Remove Kakao SDK integrity hash

--- app/page.tsx
+++ app/page.tsx
@@ -545,8 +545,6 @@ export default function WeddingPage() {
 
       <Script
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-        integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
-        crossOrigin="anonymous"
         onLoad={() => {
           const kakao = (window as any).Kakao;
           if (kakao && !kakao.isInitialized()) {
